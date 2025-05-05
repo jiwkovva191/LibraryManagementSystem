b3a5454b227e446aa9bcae7c02f9d5e230357399
jiwkovva19 <yankowadelyana@gmail.com>
2025-05-05 21:18:36 +0300

    member model

--- LibraryManagementSystem/Models/Member.cs
+++ LibraryManagementSystem/Models/Member.cs
@@ -8,5 +8,9 @@ namespace LibraryManagementSystem.Models
 {
     class Member
     {
+        public int MemberId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
